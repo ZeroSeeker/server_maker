6c7f8c18bbef6aa52eaa1be9885e95d54a464eac
caoleiming <123321>
2021-02-26 17:53:23 +0800

    添加临时测试文件

---
+++ Ubuntu/Python_3.5_installer_based_on_i386.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install build-essential
+sudo apt-get -y install checkinstall
+sudo apt-get -y install openssl
+sudo apt-get -y install libssl-dev
+sudo apt-get -y install software-properties-common
+sudo apt install -y python3-pip
+pip3 install requests -i https://mirrors.aliyun.com/pypi/simple
