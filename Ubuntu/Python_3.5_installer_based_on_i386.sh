37453ce1e449f9bfce6303222061861397a9ece2
caoleiming <123321>
2021-02-27 10:26:33 +0800

    更新

--- Ubuntu/Python_3.5_installer_based_on_i386.sh
+++ Ubuntu/Python_3.5_installer_based_on_i386.sh
@@ -7,4 +7,4 @@ sudo apt-get -y install openssl
 sudo apt-get -y install libssl-dev
 sudo apt-get -y install software-properties-common
 sudo apt install -y python3-pip
-pip3 install requests -i https://mirrors.aliyun.com/pypi/simple
+sudo pip3 install requests -i https://mirrors.aliyun.com/pypi/simple
