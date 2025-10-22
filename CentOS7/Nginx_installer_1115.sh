bdc923c250784513e40e983faf256aa5806609ca
ZeroSeeker <1260812287@qq.com>
2025-10-22 19:45:42 +0800

    更新

---
+++ CentOS7/Nginx_installer_1115.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# 升级、安装必须模块
+echo "正在安装 依赖 gcc ..."
+yum -y install gcc
+
+echo "正在安装 依赖 pcre pcre-devel ..."
+yum install -y pcre pcre-devel
+
+echo "正在安装 依赖 zlib zlib-devel ..."
+yum install -y zlib zlib-devel
+
+echo "正在安装 依赖 openssl openssl-devel ..."
+yum install -y openssl openssl-devel
+
+echo "正在下载 nginx-1.11.5.tar.gz ..."
+yum -y install wget
+wget http://nginx.org/download/nginx-1.11.5.tar.gz
+
+echo "正在创建解压目录 /usr/local/nginx_install ..."
+mkdir /usr/local/nginx_install
+
+echo "正在解压至目录 /usr/local/nginx_install ..."
+tar -zxvf nginx-1.11.5.tar.gz -C /usr/local/nginx_install
+
+echo "正在切换至目录 /usr/local/nginx_install/nginx-1.11.5/ ..."
+cd /usr/local/nginx_install/nginx-1.11.5/
+
+echo "正在编译安装 ..."
+./configure --prefix=/etc/nginx --with-http_stub_status_module --with-http_ssl_module --with-compat --with-stream
+make
+make install
+
+echo "正在创建软连接 ..."
+ln -s /etc/nginx/sbin/nginx /usr/bin/nginx
+
+echo "正在检查已安装版本 ..."
+nginx -v
+
+cd /root/server_maker/CentOS7/
+echo "正在创建开机自启动 ..."
+cp /root/server_maker/CentOS7/nginx.service /etc/systemd/system/nginx.service
+systemctl enable nginx
+systemctl start nginx.service
+systemctl status nginx.service
+
+echo ":) 完成 ..."
+
